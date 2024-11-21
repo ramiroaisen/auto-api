0a43b866bfe3c926e484dd6df3b52b0b12a7a0e8
ramiroaisen <52116153+ramiroaisen@users.noreply.github.com>
2024-11-21 12:05:05 -0300

    fix: remove Defaullt clause on custom validate

--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,7 @@
 use garde::Validate;
 
 /// garde doesnt seems to support #[garde(inner(dive))] into Vec
-pub fn dive_vec<T: Validate>(slice: &[T], ctx: &T::Context) -> Result<(), garde::error::Error>
-where
-  T::Context: Default,
-{
+pub fn dive_vec<T: Validate>(slice: &[T], ctx: &T::Context) -> Result<(), garde::error::Error> {
   // TODO: add index to path
   for item in slice {
     match item.validate_with(ctx) {
@@ -20,10 +17,7 @@ where
 
 
 /// garde doesnt seems to support #[garde(inner(dive))] into Option
-pub fn dive_option<T: Validate>(opt: &Option<T>, ctx: &T::Context) -> Result<(), garde::error::Error>
-where
-  T::Context: Default,
-{
+pub fn dive_option<T: Validate>(opt: &Option<T>, ctx: &T::Context) -> Result<(), garde::error::Error> {
   // TODO: add index to path
   if let Some(item) = opt {
     match item.validate_with(ctx) {
