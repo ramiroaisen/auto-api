5b18fafeef5108710a3181ac6215d7803d6918ac
ramiroaisen <52116153+ramiroaisen@users.noreply.github.com>
2024-11-21 15:10:50 -0300

    feat: imrpove void type to impl all types with where clauses en each fn

--- src/void.rs
+++ src/void.rs
@@ -1,18 +1,21 @@
-use std::any::{Any, TypeId};
+use std::any::{type_name, Any, TypeId};
 
-pub trait Void: Sized +'static {
-  fn is_void() -> bool {
+pub trait Void {
+  fn is_void() -> bool where Self: 'static {
     TypeId::of::<Self>() == TypeId::of::<()>()
   }
 
-  fn void() -> Self {
+  /// Create the () value from the Self = () type \
+  /// This function will panic if !Self::is_void() meaning Self is not the type ()
+  fn void() -> Self where Self: Sized + 'static {
     if Self::is_void() {
+      // this doesn't really allocate, because the size of () is 0
       let v = Box::new(()) as Box<dyn Any>;
       *v.downcast::<Self>().unwrap()
     } else {
-      panic!("cannot create void value of non void schema")
+      panic!("cannot create void value out of non void type {}", type_name::<Self>())
     }
   }
 }
 
-impl<T: Sized + 'static> Void for T {}
+impl<T> Void for T {}
